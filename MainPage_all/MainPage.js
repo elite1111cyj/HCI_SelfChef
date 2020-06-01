7e34181e290d3497b5af0ae982ed0fc711745fe1
zhyeong <z.hyeong@kaist.ac.kr>
2020-06-01 23:22:14 +0900

    JIHYEONG

---
+++ MainPage_all/MainPage.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBjFa6ITtLwyYNPwAt9YWZx0crJviZYj8g",
+    authDomain: "cs374-d.firebaseapp.com",
+    databaseURL: "https://cs374-d.firebaseio.com",
+    projectId: "cs374-d",
+    storageBucket: "cs374-d.appspot.com",
+    messagingSenderId: "774674175555",
+    appId: "1:774674175555:web:fcf3a66700feb62352a6f6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
