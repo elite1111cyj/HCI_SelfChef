9715649d900b3ad674814f3635526272bba92c7a
youngjae <elite1111cyj@gmail.com>
2020-06-02 16:41:50 +0900

    modified orderlist and connect main

--- OrderList/OrderList.js
+++ OrderList/OrderList.js
@@ -1,11 +1,20 @@
 // This allows the Javascript code inside this block to only run when the page
 // has finished loading in the browser.
 $( document ).ready(function() {
-  $(".deleteBtn").click(function(){ // click button event
-    alert("delete btn clicked!");
+
+  $(document).on("click", ".deleteBtn", function() { //del button click events
+    var n = this.parentNode.id;
+    console.log("delete", n);
+    var check = confirm("Are you sure to cancel "+n+"?");
+    if(check){
+      $("#"+n).remove();
+    }
   });
+
   $(".ongoingProduct").mouseover(function(){$(this).css("box-shadow", "0px 0px 22px -6px gray");});
   $(".ongoingProduct").mouseleave(function(){$(this).css("box-shadow", "0px 0px 0px 0px gray");});
+
+
 /*
   $(document).on('mouseenter', '.ongoingProduct', function(e) {
     $(e.target).attr('style', "box-shadow: 0px 0px 22px -6px gray");
