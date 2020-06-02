09dd9846edbed7b8985a8f040dc3294900908da5
Sangwoo Kim <bryanswkim@gmail.com>
2020-06-02 20:38:22 +0900

    Deal Cancel / Deal Success

---
+++ DP - Deal Cancel/deal_cancel.js
@@ -0,0 +1,5 @@
+// Open the popup
+function dealCancel() {
+  var dealCancelPopup = document.getElementById("dealCancelPopup");
+  dealCancelPopup.classList.toggle("showPopup");
+}
---
+++ DP - Deal Success/deal_success.js
@@ -0,0 +1,5 @@
+// Open the popup
+function dealSuccess() {
+  var dealSuccessPopup = document.getElementById("dealSuccessPopup");
+  dealSuccessPopup.classList.toggle("showPopup");
+}
