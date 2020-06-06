586368143991f33a73f62c9210168b5829f69354
zhyeong <z.hyeong@kaist.ac.kr>
2020-06-06 10:34:52 +0900

    shared js

---
+++ DP4_all.js
@@ -0,0 +1,7 @@
+//[START] JIHYEONG
+
+var now_ID; //ID of the current user
+var search_key; // search keyword typed in. string ex) "carrot" if present, null if not present
+var search_category; //category chosen. string ex) "vegetable", "fruit" if present, null if not present
+
+//[END] JIHYEONG
